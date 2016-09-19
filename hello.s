9556fe6ba73d2bfcb2108fc45f198e1f32b1e3c5
zhiyuan zhu <zzyjsjcom@126.com>
2016-09-19 21:59:54 +0800

    add code

---
+++ hello.s
@@ -0,0 +1,7 @@
+	.text
+	.global add
+add: 
+	mov r1, #1
+	mov r2, #2
+	add r0, r1, r2
+	bx lr
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("test: add(1+2)=%d\n", add(1+2));
+	return 0;
+}
